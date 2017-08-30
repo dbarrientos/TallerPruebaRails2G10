019377de430b3ab77ae337c2398ae1529db6ef57
David Barrientos <david.barrientos.f@gmail.com>
2017-08-30 16:44:53 -0300

    Hicimos varias cosas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'products/index'
+  post 'products/index'
+
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
