87ef1e7aaca898043ad1cb580538b8263cf67077
David Barrientos <david.barrientos.f@gmail.com>
2017-08-30 14:39:56 -0300

    agregamos premium y precio a product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :price, presence: true
+
+  # before_save :premium_default
+
+  # def premium_default
+  #   if self.premium.nil?
+  #     self.premium = false
+  #   end
+  # end
 end
